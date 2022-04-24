650e4119113443b7efdb00723c05a7d4c4579cda
weaselflink <stefan@stefanbissell.de>
2022-04-24 16:02:20 +0200

    add another example

---
+++ examples/Fading_Suns_Into_the_Dark.kts
@@ -0,0 +1,12 @@
+import de.stefanbissell.pdfeasyoutline.outline
+
+outline("Fading Suns - Into the Dark") {
+    entry("Alustro's Journel: Ghost Story", 7)
+    entry("Introduction: Casting Light", 10)
+    entry("Thorn Manor", 10)
+    entry("Dark Liner", 43)
+    entry("Love's Labors", 51)
+    entry("The Maltese Gargoyle", 65)
+    entry("The Ancestor's Tomb", 81)
+    entry("Short Dramas", 106)
+}
