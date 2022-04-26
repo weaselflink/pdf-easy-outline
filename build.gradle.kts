7d24eca0d9cc0df834f520182133fc38a441a6a3
weaselflink <stefan@stefanbissell.de>
2022-04-26 12:50:52 +0200

    fix running script engine from jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,11 +23,13 @@ dependencies {
     implementation("org.slf4j:slf4j-nop:1.7.36")
 
     implementation(kotlin("stdlib"))
-    implementation(kotlin("reflect"))
-    implementation(kotlin("script-runtime"))
-    implementation(kotlin("compiler-embeddable"))
-    implementation(kotlin("script-util"))
+    runtimeOnly(kotlin("script-runtime"))
+    runtimeOnly(kotlin("scripting-common"))
+    runtimeOnly(kotlin("scripting-jvm"))
+    runtimeOnly(kotlin("scripting-jvm-host"))
+    runtimeOnly(kotlin("compiler-embeddable"))
     runtimeOnly(kotlin("scripting-compiler-embeddable"))
+    runtimeOnly(kotlin("scripting-jsr223"))
 }
 
 application {
--- src/main/kotlin/de/stefanbissell/pdfeasyoutline/Main.kt
+++ src/main/kotlin/de/stefanbissell/pdfeasyoutline/Main.kt
@@ -2,10 +2,14 @@ package de.stefanbissell.pdfeasyoutline
 
 import java.nio.file.Path
 import javax.script.ScriptEngineManager
+import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
-    val configPath = args[0]
-    val inputPath = args[1]
+    if (args.size != 3) {
+        printUsage()
+    }
+    val configPath = args[0].checkExists()
+    val inputPath = args[1].checkExists()
     val outputPath = args[2]
 
     val config = loadConfig(configPath)
@@ -17,6 +21,21 @@ fun main(args: Array<String>) {
 private fun loadConfig(path: String): OutlineEntry {
     val script = Path.of(path).toFile().readText()
     val engine = ScriptEngineManager().getEngineByExtension("kts")
+    requireNotNull(engine) { "Could not load kts script engine" }
     engine.eval("import de.stefanbissell.pdfeasyoutline.outline\n")
     return engine.eval(script) as OutlineEntry
+}
+
+private fun printUsage() {
+    println("Usage:")
+    println("java -jar pdf-easy-outline-1.0-all.jar <outline-file> <input-file> <output-file>")
+    exitProcess(1)
+}
+
+private fun String.checkExists(): String {
+    if (!Path.of(this).toFile().exists()) {
+        println("Could not find $this")
+        exitProcess(1)
+    }
+    return this
 }
