0f08982ef7b708ee557bc439e74450b788ff004b
weaselflink <stefan@stefanbissell.de>
2022-05-28 21:24:55 +0200

    fix example

--- examples/Fading_Suns_Into_the_Dark.kts
+++ examples/Fading_Suns_Into_the_Dark.kts
@@ -2,7 +2,7 @@
 outline("Fading Suns - Into the Dark") {
     entry("Alustro's Journel: Ghost Story", 7)
     entry("Introduction: Casting Light", 10)
-    entry("Thorn Manor", 10)
+    entry("Thorn Manor", 12)
     entry("Dark Liner", 43)
     entry("Love's Labors", 51)
     entry("The Maltese Gargoyle", 65)
